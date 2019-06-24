c0353513c52a555de935212d50698ae1a912675f
Adam Ervin <adamjervin@gmail.com>
2019-06-24 21:17:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+if name_hash.collect do |name|
+  name == 1 
+  return name
+  
+end
+end
 end
