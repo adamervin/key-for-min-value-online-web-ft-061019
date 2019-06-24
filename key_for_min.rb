8db4298be33d880d470a2b056a87c15574b1b60d
Adam Ervin <adamjervin@gmail.com>
2019-06-24 21:31:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  val = 0 
 name_hash.collect do |name,value|
   if value < 2
      name
    else 
-     nil
+     val
 
 
 
